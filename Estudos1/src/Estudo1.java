53f9e482f8b0499cee9f5ed71612e51ed1e59ef1
Thiago Fagundes Ataides <110539786+tfagundes6@users.noreply.github.com>
2022-08-08 13:26:47 -0300

    Basic Java

---
+++ Estudos1/src/Estudo1.java
@@ -0,0 +1,80 @@
+
+public class Estudo1 {
+
+	public static void main(String[] args) {
+		
+		
+		/*
+		 * VARIAVEIS E TIPOS PRIMITIVOS 
+		 */
+		
+		int idade;
+		idade = 15;
+		
+		// Calcular idade no ano seguinte
+		
+		idade = idade +1;
+		
+		System.out.println(idade);
+	
+		//Operadores 
+		
+		int quatro = 2+2;
+		int cinco = 4-1;
+		int oito = 4*2;
+		int dois = 8/4;
+		int um = 5%2;
+		
+		System.out.println(quatro);
+		System.out.println(cinco);
+		System.out.println(oito);
+		System.out.println(dois);
+		System.out.println(um);
+		
+		// Tipo double 
+		
+		double pi = 3.14;
+		double a = 5*2;
+		
+		
+		//Boolean
+		
+		boolean verdade = true;
+		boolean mentira = false;
+		
+		
+		idade = 15;
+		boolean menorIdade = idade < 18;
+		
+		System.out.println(menorIdade);
+		
+		// Tipo Char
+		
+		char letra = 'a';
+		
+		System.out.println(letra);
+		
+		double mes1 = 15.000;
+		double mes2 = 23.000;
+		double mes3 = 17.000;
+		
+		double media = (mes1 + mes2 + mes3) /3;
+		double soma = mes1 + mes2 + mes3;
+		
+		System.out.println("Soma " + soma);
+		System.out.println("Media " + media);
+		
+		
+		
+	}
+
+		
+	
+	
+	
+	
+	
+	
+	
+
+}
